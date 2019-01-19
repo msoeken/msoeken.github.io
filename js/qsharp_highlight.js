0f4c9d1661f671e74b5f7abf133d9ebc44a7ef44
Mathias Soeken <mathias.soeken@epfl.ch>
2019-01-19 10:28:55 -0700

    Update.

--- js/qsharp_highlight.js
+++ js/qsharp_highlight.js
@@ -1,9 +1,9 @@
 hljs.registerLanguage("qsharp", function(hljs) {
   return {
       keywords: {
-          keyword: 'operation function let using for if in',
+          keyword: 'operation function let using for if in body adjoint',
           type: 'Int Qubit Bool Unit',
-          built_in: 'H HY RFrac CNOT PauliZ'
+          built_in: 'H HY RFrac CNOT PauliZ M Reset'
       },
       contains: [
           hljs.C_LINE_COMMENT_MODE,
