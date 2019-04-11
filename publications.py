2ea00560dbfab300522ae6e872ddf081eeba41f8
Mathias Soeken <mathias.soeken@epfl.ch>
2019-04-11 09:10:58 +0200

    Update.

--- publications.py
+++ publications.py
@@ -980,8 +980,8 @@ article_data = [
     ( ['ok', 'ms', 'rd'],                         'ipl',         139,  "",       2018, 'The complexity of error metric',                                                                                                         '1--7',       'https://doi.org/10.1016/j.ipl.2018.06.010' ),
     ( ['ms', 'et', 'am', 'gdm'],                  'ipl',         139,  "",       2018, 'Pairs of majority-decomposing functions',                                                                                                '35-38',      'https://doi.org/10.1016/j.ipl.2018.07.004' ),
     ( ['et', 'ms', 'la', 'gdm'],                  'procieee',    107,  "1",      2019, 'Logic synthesis for established and emerging computing',                                                                                 '1558--2256', 'https://doi.org/10.1109/JPROC.2018.2869760' ),
+    ( ['et', 'ms', 'la', 'wh', 'gdm'],            'tc',           68,  "5",      2019, 'Mapping monotone Boolean functions into majority',                                                                                       '791--797',   'https://doi.org/10.1109/TC.2018.2881245' ),
     ( ['ms', 'mr', 'nw', 'gdm'],                  'tcad',         -1,  "",          0, 'LUT-based hierarchical reversible logic synthesis',                                                                                      '',           'https://doi.org/10.1109/TCAD.2018.2859251' ),
-    ( ['et', 'ms', 'la', 'wh', 'gdm'],            'tc',           -1,  "",          0, 'Mapping monotone Boolean functions into majority',                                                                                       '',           'https://doi.org/10.1109/TC.2018.2881245' ),
     ( ['wh', 'ms', 'am', 'gdm'],                  'tcad',         -1,  "",          0, 'SAT-based exact synthesis: encodings, topology families, and parallelism',                                                               '',           'https://doi.org/10.1109/TCAD.2019.2897703' )
 ]
 
@@ -1010,7 +1010,7 @@ news_data = [
     ( 'aspdac', 2019 ),
     ( 'procieee', 107 ),
     ( 'date', 2019 ),
-    ( 'tc', -1 ),
+    ( 'tc', 68 ),
     ( 'tcad', -1 ),
     ( 'iscas', 2019 ),
     ( 'ismvl', 2019 ),
